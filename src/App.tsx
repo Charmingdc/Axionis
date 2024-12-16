dffa8d9d9e9eec8833311b25d099063c707437a9
Charmingdc <charmingdc002@gmail.com>
2024-12-16 02:35:06 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
+import AxionisImg from '/public/axionis.jpg'
 import PWABadge from './PWABadge.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="Axionis logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Axionis</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <section className="container">
+        <ul className="topbar">
+         <li>
+          <img 
+           src={AxionisImg} 
+           alt="Axionis" 
+           id="axionis-dp" />
+           
+          <h3 id="axionis-name">
+            Axionis
+          </h3>
+        </li>
+        <li>
+          <i className="fa-solid fa-ellipsis-vertical" id="three-dot"></i>
+        </li>
+       </ul>
+       
+       
+       <ul className="action-tab">
+         <li>
+           <i className="fa-solid fa-circle-user"></i> Developer
+         </li>
+         <li>
+           <i className="fa-solid fa-info-circle"></i> View details
+         </li>
+         <li>
+           <i className="fa-solid fa-edit"></i> New chat
+         </li>
+       </ul>
+       
+       
+       <div className="chatbox">
+        
+        <div className="default-screen">
+           <img 
+             src={AxionisImg}
+             alt="Axionis Chatbot" />
+           <h3>
+             Axionis
+           </h3>
+           <p>
+             Hi, I'm Axionis. Your friendly ai chatbot.
+           </p>
+        </div>
+        
+       </div>
+      
+      
+       <div className="chat-sender">
+        <input 
+          type="text"
+          value={() => {}}
+          placeholder="Send a message" />
+          
+        <button className="send-button">
+         <i className="fa-solid fa-paper-plane" id="send-icon"></i>
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       </div>
+       
+       
+      </section>
+      
       <PWABadge />
     </>
   )
